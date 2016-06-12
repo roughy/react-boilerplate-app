59ecc751c8e4ceffbb1207d0b0139686f5b0d176
Marco Reinwarth <marcoreinwarth@gmail.com>
2016-06-12 21:53:03 +0200

    fine tuning the react components

--- src/app/Counter.jsx
+++ src/app/Counter.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+/**
+ * Counter component, displaying the current counter and a button for incrementation
+ */
 class Counter extends React.Component {
 
+  // construct the counter
   constructor(props) {
     super(props);
     const start = typeof props.start !== 'undefined' ? parseInt(props.start) : 0;
@@ -9,11 +13,20 @@ class Counter extends React.Component {
     this.onClick = this.onClick.bind(this);
   }
 
+  /**
+   * handles the button click
+   * increment the current count
+   */
   onClick () {
     let newCount = this.state.count + 1;
     this.setState({count: newCount});
   }
 
+  /**
+   * renders the view
+   * 
+   * @returns {XML}
+   */
   render() {
     return (
       <div>
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,15 +3,19 @@ import {render} from 'react-dom';
 
 import CounterComponent from './Counter.jsx';
 
+/**
+ * Application start point
+ */
 class App extends React.Component {
   render () {
     return (
       <div>
         <p>Hello fellow little counter...</p>
-        <CounterComponent start="43" />
+        <CounterComponent />
       </div>
     );
   }
 }
 
+// Render application components into root element
 render(<App/>, document.getElementById('app'));
